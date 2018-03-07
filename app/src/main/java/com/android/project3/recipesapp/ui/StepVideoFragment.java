d29078aca559c8bf4fd31d22d63bf65175b5f670
Karina Bernice <karina.w.kiba@gmail.com>
2018-03-07 10:23:48 -0300

    Added ExoPlayer to StepDetail Fragment

--- app/src/main/java/com/android/project3/recipesapp/ui/StepVideoFragment.java
+++ app/src/main/java/com/android/project3/recipesapp/ui/StepVideoFragment.java
@@ -1,5 +1,6 @@
 package com.android.project3.recipesapp.ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,7 +11,18 @@ import android.widget.TextView;
 
 import com.android.project3.recipesapp.R;
 import com.android.project3.recipesapp.data.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 
 /**
@@ -18,8 +30,9 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
  */
 
 public class StepVideoFragment extends Fragment {
-    TextView mVideoUrlTextView;
+    TextView mStepFullDescr;
     SimpleExoPlayerView mExoPlayerView;
+    SimpleExoPlayer mExoPlayer;
     Step mStep;
 
     public StepVideoFragment(){
@@ -31,10 +44,12 @@ public class StepVideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.step_video_fragment, container, false);
 
-        mExoPlayerView = (SimpleExoPlayerView)rootView.findViewById(R.id.sep_test_video_url);
+        mStepFullDescr = (TextView)rootView.findViewById(R.id.tv_step_full_description);
+        mExoPlayerView = (SimpleExoPlayerView)rootView.findViewById(R.id.player_view);
 
         if(mStep != null){
-            mVideoUrlTextView.setText(mStep.getVideoURL()+"\n"+mStep.getDescription());
+            mStepFullDescr.setText(mStep.getDescription());
+            initializePlayer(mStep.getVideoURL());
             return rootView;
         }
 
@@ -43,5 +58,37 @@ public class StepVideoFragment extends Fragment {
 
     public void setStepData(Step step){
         mStep = step;
+    }
+
+    private void initializePlayer(String mediaStringUri){
+        Uri mediaUri = prepareStepUri(mediaStringUri);
+        if (mExoPlayer == null){
+            /**create an instance of the ExoPlayer**/
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mExoPlayerView.setPlayer(mExoPlayer);
+            /**prepare the media source**/
+            String userAgent = Util.getUserAgent(getContext(), "RecipesApp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private Uri prepareStepUri(String stepStringUri){
+        return Uri.parse(stepStringUri).buildUpon().build();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
     }
 }
