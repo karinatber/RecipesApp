0447d307f840633561f8e419dc450d936baff71c
Karina Bernice <karina.w.kiba@gmail.com>
2018-03-27 22:23:50 -0300

    Started implementing test

--- app/src/androidTest/java/com/android/project3/recipesapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/android/project3/recipesapp/ExampleInstrumentedTest.java
@@ -2,18 +2,34 @@ package com.android.project3.recipesapp;
 
 import android.app.Instrumentation;
 import android.content.Context;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
+
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
 
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.EspressoKey;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.android.exoplayer2.util.Assertions;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import java.util.regex.Matcher;
+
+import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -24,11 +40,6 @@ import static org.junit.Assert.assertEquals;
 public class ExampleInstrumentedTest {
     Context mAppContext;
     Instrumentation mInstrumentation;
-
-    @Before
-    public void setConfig() {
-        mAppContext = InstrumentationRegistry.getTargetContext();
-    }
 
     @Before
     public void setUpTest(){
@@ -46,10 +57,6 @@ public class ExampleInstrumentedTest {
 
     @Test
     public void clickRecipeTest(){
-
-    }
-
-    public void doAllTests(){
 
     }
 
