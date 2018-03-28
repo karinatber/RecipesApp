19927b892e8546d2e40ba363c23c3728753e7699
Karina Bernice <karina.w.kiba@gmail.com>
2018-03-27 22:23:50 -0300

    Started implementing test

--- app/src/androidTest/java/com/android/project3/recipesapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/android/project3/recipesapp/ExampleInstrumentedTest.java
@@ -1,14 +1,35 @@
 package com.android.project3.recipesapp;
 
+import android.app.Instrumentation;
 import android.content.Context;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.EspressoKey;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
+<<<<<<< HEAD
+=======
+import com.google.android.exoplayer2.util.Assertions;
+
+import org.junit.Assert;
+>>>>>>> 3248a60... Started implementing test
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+<<<<<<< HEAD
 import static org.junit.Assert.assertEquals;
+=======
+import java.util.regex.Matcher;
+
+import static org.junit.Assert.*;
+>>>>>>> 3248a60... Started implementing test
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -17,12 +38,22 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+<<<<<<< HEAD
     private Context mAppContext;
 
     @Before
     public void setConfig(){
         mAppContext = InstrumentationRegistry.getTargetContext();
 
+=======
+    Context mAppContext;
+    Instrumentation mInstrumentation;
+
+    @Before
+    public void setUpTest(){
+        mAppContext = InstrumentationRegistry.getTargetContext();
+        mInstrumentation = InstrumentationRegistry.getInstrumentation();
+>>>>>>> 3248a60... Started implementing test
     }
 
     @Test
@@ -34,8 +65,20 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
+<<<<<<< HEAD
     public void clickRecipeTest(){
 
     }
 
+=======
+    public void doAllTests(){
+
+    }
+
+    @Test
+    public void clickOnRecipeItemAndCheckDetailsTest(){
+        Espresso.onData(ViewMatchers.withId(R.id.rv_recipes_list)).atPosition(0).perform(ViewActions.click());
+    }
+
+>>>>>>> 3248a60... Started implementing test
 }
